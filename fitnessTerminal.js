948f44a12aeef62614b07a1a59077fe17f572b39
Noah <noahmichaels@gmail.com>
2022-09-07 23:02:11 -0400

    a few changes were made: 1. cleaned up code 2. Added Comments 3. fixed issue with prompt causing multiple repeats when getting input

--- fitnessTerminal.js
+++ fitnessTerminal.js
@@ -2,9 +2,24 @@
 const prompt = require('prompt-sync')();
 
 //Welcome the user and ask them to choose what they want to do
-let selection = prompt(`Hello! what would you like to do?
-1. View/Edit Goals
-2. View/Edit Runs
-`);
-console.log(`You chose ${selection}`);
+console.log('Hello! what would you like to do? \n 1. View/Edit Goals \n 2. View/Edit Workout');
+//prompt the user to input: looking for Goals, Workout, 1 or 2
+let selection = prompt();
+//error check: console.log(`You chose ${selection}`);
+//selection = Number(selection);
 
+switch(selection){
+    case 1:
+        goals();
+        break;
+    case 2:
+        workout();
+        break;
+}
+
+function goals(){
+    console.log('You made it to the Goals Function');
+}
+function workout(){
+    console.log('You made it to the Workout Funciton');
+}
