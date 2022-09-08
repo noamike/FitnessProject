f4693ffdc9a6a8076adf13592993c021aff1e9fe
Noah <noahmichaels@gmail.com>
2022-09-08 14:26:36 -0400

    Update fitnessTerminal.js
    Set the Main Question to a function to allow user to go back when inside other functions
    
    created the menu options for goals()

--- fitnessTerminal.js
+++ fitnessTerminal.js
@@ -2,26 +2,30 @@ const prompt = require("prompt-sync")();
 let selectionChoice = false;
 
 function goals() {
-    console.log("You made it to the Goals Function");
+    //console.log("You made it to the Goals Function");
     selectionChoice = true;
+    console.log("My Goals: \n What would you like to do? \n 1. View Goals \n 2. Edit Goals \n 3. Return home");
 }
 function workout() {
     console.log("You made it to the Workout Function");
     selectionChoice = true;
 }
+function mainScreen(){
+    do {
+        //prompt the user to input: looking for Goals, Workout, 1 or 2
+        console.log(
+            "Hello! what would you like to do? \n 1. View/Edit Goals \n 2. View/Edit Workout"
+        );
+        let selection = prompt();
+        
+        if (selection == "1" || selection == "goals") {
+            goals();
+        } else if (selection == "2" || selection == "workout") {
+            workout();
+        } else {
+            console.log("Im sorry, please enter 1, 2, goals, or workout");
+        }
+    } while (!selectionChoice);
+}
 
-do {
-    //prompt the user to input: looking for Goals, Workout, 1 or 2
-    console.log(
-        "Hello! what would you like to do? \n 1. View/Edit Goals \n 2. View/Edit Workout"
-    );
-    let selection = prompt();
-    
-    if (selection == "1" || selection == "goals") {
-        goals();
-    } else if (selection == "2" || selection == "workout") {
-        workout();
-    } else {
-        console.log("Im sorry, please enter 1, 2, goals, or workout");
-    }
-} while (!selectionChoice);
+mainScreen();
