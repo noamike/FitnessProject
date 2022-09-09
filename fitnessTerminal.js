c1edda2a7984a52675f91a2582c34cd95024b67f
Noah <noahmichaels@gmail.com>
2022-09-09 14:38:44 -0400

    Update fitnessTerminal.js
    added breakdown for goals selection along with a Goodbye option from main screen

--- fitnessTerminal.js
+++ fitnessTerminal.js
@@ -4,17 +4,32 @@ let selectionChoice = false;
 function goals() {
     //console.log("You made it to the Goals Function");
     selectionChoice = true;
-    console.log("My Goals: \n What would you like to do? \n 1. View Goals \n 2. Edit Goals \n 3. Return home");
+    selectionChoice = false;
+    do{
+        console.log("My Goals: \n What would you like to do? \n 1. View Goals \n 2. Edit Goals \n 3. Return home");
+        let goalSelection = prompt();
+        if (goalSelection == "1" || goalSelection == 'view'){
+
+        } else if (goalSelection == "2" || goalSelection == "edit"){
+
+        } else if (goalSelection == "3" || goalSelection == "home"){
+            mainScreen();
+        }else {
+            console.log("Im sorry, please enter 1, 2, 3, view, edit, or home")
+        }
+    }while (!selectionChoice);
 }
 function workout() {
     console.log("You made it to the Workout Function");
     selectionChoice = true;
 }
 function mainScreen(){
+    selectionChoice = true;
+    selectionChoice = false;
     do {
         //prompt the user to input: looking for Goals, Workout, 1 or 2
         console.log(
-            "Hello! what would you like to do? \n 1. View/Edit Goals \n 2. View/Edit Workout"
+            "Hello! what would you like to do? \n 1. View/Edit Goals \n 2. View/Edit Workout \n 3. Exit"
         );
         let selection = prompt();
         
@@ -22,10 +37,14 @@ function mainScreen(){
             goals();
         } else if (selection == "2" || selection == "workout") {
             workout();
-        } else {
-            console.log("Im sorry, please enter 1, 2, goals, or workout");
+        } else if (selection == "3" || selection == "exit"){
+            console.log("Goodbye!");
+            selectionChoice = true;
+        }else {
+            console.log("Im sorry, please enter 1, 2, 3, goals, workout, or exit");
         }
     } while (!selectionChoice);
+    return;
 }
 
 mainScreen();
